605722e1444b34e50847db282c005e9a6a3c20ed
Christian Nuñez del Prado Rodriguez <cdnunez@minsait.com>
2024-03-10 15:04:56 +0100

    AP-33 pipeline for destroying infrastructure as code was updated with new commands at main script

--- pro/destroy-architecture.sh
+++ pro/destroy-architecture.sh
@@ -1,26 +1,5 @@
-echo Destroying logic layer of frontend
-cd 05_frontend
-terraform init
-terraform workspace list
-terraform workspace select workshop1-pro-frontend
-terraform destroy -var-file="../variables.tfvars" -auto-approve
-
-echo Destroying logic layer of kubernetes
-cd ../04_kubernetes
-terraform init
-terraform workspace list
-terraform workspace select workshop1-pro-kubernetes
-terraform destroy -var-file="../variables.tfvars" -auto-approve
-
-echo Destroying logic layer of database
-cd ../03_database
-terraform init
-terraform workspace list
-terraform workspace select workshop1-pro-database
-terraform destroy -var-file="../variables.tfvars" -auto-approve
-
 echo Destroying logic layer of bastion
-cd ../02_bastion
+cd 02_bastion
 terraform init
 terraform workspace list
 terraform workspace select workshop1-pro-bastion
